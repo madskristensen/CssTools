3b506ac7cbf7b91b2e0840dc93e54f3337f174b4
Mads Kristensen <post@madskristensen.net>
2016-11-28 10:29:59 -0800

    Color adornments

--- README.md
+++ README.md
@@ -14,18 +14,10 @@ See the [change log](CHANGELOG.md) for changes and road map.
 
 ## Features
 
-- Feature 1
-- Feature 2
-  - Sub feature
+- Color adornments
 
-### Feature 1
-Describe feature 1. Add screenshots/code samples etc.
-
-### Feature 2
-Describe feature 2. Add screenshots/code samples etc.
-
-#### Sub feature
-Describe sub feature. Add screenshots/code samples etc.
+### Color adornments
+coming soon...
 
 ## Contribute
 Check out the [contribution guidelines](.github/CONTRIBUTING.md)
--- src/CssToolsPackage.cs
+++ src/CssToolsPackage.cs
@@ -1,31 +1,19 @@
-﻿using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
+﻿using Microsoft.VisualStudio.Shell;
+using System;
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
 
 namespace CssTools
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
-    [Guid(CssToolsPackage.PackageGuidString)]
+    [Guid(PackageGuidString)]
     public sealed class CssToolsPackage : Package
     {
         public const string PackageGuidString = "8a85e089-0667-415f-a01c-f2f7564e7bac";
 
-        public CssToolsPackage()
-        {
-        }
-
         protected override void Initialize()
         {
-            base.Initialize();
+
         }
     }
 }
