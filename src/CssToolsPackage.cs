4c6d712b985b4f210f4f13b5fd3e5ed1a6df3f89
Mads Kristensen <post@madskristensen.net>
2018-08-16 08:44:36 -0700

    Updated to AsyncPackage

--- src/CssToolsPackage.cs
+++ src/CssToolsPackage.cs
@@ -4,16 +4,10 @@ using System.Runtime.InteropServices;
 
 namespace CssTools
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
-    [Guid(PackageGuidString)]
-    public sealed class CssToolsPackage : Package
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version, IconResourceID = 400)]
+    [Guid("8a85e089-0667-415f-a01c-f2f7564e7bac")]
+    public sealed class CssToolsPackage : AsyncPackage
     {
-        public const string PackageGuidString = "8a85e089-0667-415f-a01c-f2f7564e7bac";
-
-        protected override void Initialize()
-        {
-
-        }
     }
 }
